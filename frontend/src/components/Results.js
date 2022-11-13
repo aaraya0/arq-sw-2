20a30695df7a870f7872680aa87bc81f71af0866
aaraya <agusaraya131@gmail.com>
2022-11-13 20:23:44 -0300

    cambios front

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -1,73 +1,68 @@
+import React, { useState , useEffect} from "react";
 
-import React, {useEffect, useState}  from "react";
+import Cookies from "universal-cookie";
 
-import Cookies from 'universal-cookie';
-import "./Results.css";
+
+
 const cookies = new Cookies();
-async function getSearch(query){
-    return (fetch("http://localhost:8983/solr/publicaciones/select?" + query + "=&defType=lucene&omitHeader=true&indent=true&q.op=AND&q=*%3A*")
-    .then(response => response.json()));
-     }
 
-async function getStuff(){
-    let items= await getSearch(cookies.get("busqueda_limpia"))
-    console.log(items)
-           
-            return items
+async function getItems(){
+    let query= cookies.get("busqueda_limpia")
+  return await fetch('http://localhost:8983/solr/publicaciones/select?&defType=lucene&indent=true&q=description:"'+query+'"title:"'+query+'"&q.op=OR')
+.then(response => response.json())
 }
+//fetch("htttp://localhost:8090/search"+query)
+
+
+
+
+
+function showItems(items){
+  return items.map((item) =>
+
+   <div obj={item} key={item.id} className="item">
    
-
-
-function gopath(path){
-    window.location = window.location.origin + path
-  }
+    <a className="title">{item.title}</a>
+    <a className="price"> {"$" + item.price}</a>
 
   
+    <div>
+      <a className="location">{item.city},</a>
+    </div>
+    <div>
+      <a className="description">{item.description}</a>
+    </div>
+    <div className="sellerBlock">
+      <a className="seller">{item.seller}</a>
+    </div>
 
-class Results extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-        items: [],
-        DataisLoaded: true
-        };
-        }
-       
-        
-
-        componentDidMount() {
-            this.setState(getStuff())
-            }
-
-            render() {
-            const { DataisLoaded, items} = this.state;
-                if (!DataisLoaded) return <div>
-                <h1> Please wait... </h1> </div> ;
-            
-            
-            const publi= items.map((item) => (
-            
-                <div id = { item.id } className="item">
-                    <div id="titulo">{ item.title}</div>
-                    <div id="desc"> { item.description }</div>	
-                    <div id="seller"> { item.seller }</div>	
-                    <div id="city">{ item.city }</div>
-                    <div id="state">{ item.state }</div>
-              </div>
-               
-            
-            ))
-         
-            return (
-                <div >
-                
-               
-               
-                <div className="public">{publi}</div>
-                </div>
-                )}
-            
-            
-
+   </div>
+ )
 }
+
+
+
+function Results() {
+  const [items, setItems] = useState([])
+ 
+  getItems().then(response => setItems(response))
+
+  
+    
+  
+
+  return (
+    <div className="home">
+  
+
+     
+
+      <div id="main">
+       {showItems(items)}
+
+      </div>
+    </div>
+  );
+}
+
 export default Results;
